22c076be3164abbde2f8ad2c18f3bed0ab1a39de
Barshan Dey <itsbarshandey@gmail.com>
2025-09-02 10:47:22 +0000

    how to use github to publish  this website

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/nextn',
+  assetPrefix: '/nextn/',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
